717671c6de5f5b6619be5c9b67ec4f278e1a45a6
Théo FIDRY <theo.fidry@gmail.com>
2018-01-26 20:46:49 +0000

    Add working-dir option (#41)
    Add the option `working-dir|d` to the `build` command in order to
    execute the build command from a different directory.

---
+++ src/Command/ChangeableWorkingDirectory.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the box project.
+ *
+ * (c) Kevin Herrera <kevin@herrera.io>
+ *     Théo Fidry <theo.fidry@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace KevinGH\Box\Command;
+
+use Assert\Assertion;
+use Symfony\Component\Console\Exception\RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+
+trait ChangeableWorkingDirectory
+{
+    /**
+     * @internal using a static property as traits cannot have constants
+     */
+    private static $WORKING_DIR_OPT = 'working-dir';
+
+    final public function changeWorkingDirectory(InputInterface $input): void
+    {
+        $workingDir = $input->getOption(self::$WORKING_DIR_OPT);
+
+        if (null === $workingDir) {
+            return;
+        }
+
+        Assertion::directory(
+            $workingDir,
+            'Could not change the working directory to "%s": directory does not exists or file is not a directory.'
+        );
+
+        if (false === chdir($workingDir)) {
+            throw new RuntimeException(
+                sprintf(
+                    'Failed to change the working directory to "%s" from "%s".',
+                    $workingDir,
+                    getcwd()
+                )
+            );
+        }
+    }
+
+    final private function configureWorkingDirOption(): void
+    {
+        $this->addOption(
+            self::$WORKING_DIR_OPT,
+            'd',
+            InputOption::VALUE_REQUIRED,
+            'If specified, use the given directory as working directory.',
+            null
+        );
+    }
+}
