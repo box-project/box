c18ff9e6346198960c8a0a5112268639a1d95de5
Théo FIDRY <theo.fidry@gmail.com>
2018-04-07 22:23:41 +0200

    Fix CS

--- src/Console/Command/Build.php
+++ src/Console/Command/Build.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 
 namespace KevinGH\Box\Console\Command;
 
-use KevinGH\Box\Box;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
