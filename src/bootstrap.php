dd81fff25d9981b27bc10827b57cb101ae7f1807
Théo FIDRY <theo.fidry@gmail.com>
2018-04-23 22:51:43 +0100

    Fix the parallel processing in a renamed PHAR
    When the PHAR was renamed, the autoload files could no longer be found.
    
    Closes #152

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -14,47 +14,80 @@ declare(strict_types=1);
 
 namespace KevinGH\Box;
 
+use function array_filter;
 use ErrorException;
+use function get_declared_classes;
+use function preg_match;
 use RuntimeException;
+use function substr;
+use function var_dump;
 
 $findAutoloader = function () {
-    if (file_exists($autoload = __DIR__.'/../../../autoload.php')) {
-        // Is installed via Composer
-        return $autoload;
-    }
-
-    if (file_exists($autoload = __DIR__.'/../vendor/autoload.php')) {
-        // Is installed locally
-        return $autoload;
-    }
-
-    throw new RuntimeException('Unable to find the Composer or PHP-Scoper autoloader.');
+//    if (file_exists($autoload = __DIR__.'/../../../autoload.php')) {
+//        // Is installed via Composer
+//        return $autoload;
+//    }
+//
+//    if (file_exists($autoload = __DIR__.'/../vendor/autoload.php')) {
+//        // Is installed locally
+//        return $autoload;
+//    }
+//
+//    if ('phar:' === substr(__FILE__, 0, 5)
+//        && file_exists($autoload = 'phar://box.phar/vendor/autoload.php')
+//    ) {
+//        // Is in the PHAR
+//        return $autoload;
+//    }
+//
+//    var_dump(array_filter(get_declared_classes(), function ($class) {
+//        return 1 === preg_match('/Composer/', $class);
+//    }));die;
+//
+//    throw new RuntimeException('Unable to find the Composer autoloader.');
 };
 
 // TODO: update PHP-Scoper to get rid of this horrible hack at some point
-$findPhpScoperFunctions = function () {
+$findPhpScoperFunctions = function (): void {
     if (file_exists($autoload = __DIR__.'/../../php-scoper/src/functions.php')) {
         // Is installed via Composer
-        return $autoload;
+        require_once $autoload;
+
+        return;
     }
 
     if (file_exists($autoload = __DIR__.'/../vendor/humbug/php-scoper/src/functions.php')) {
         // Is scoped (in PHAR or dumped directory) or is installed locally
-        return $autoload;
+        require_once $autoload;
+
+        return;
+    }
+
+    if ('phar:' === substr(__FILE__, 0, 5)) {
+        // Is in the PHAR but the PHAR has been renamed without the extension `.phar`. As a result the PHAR protocol
+        // `phar://path/to/file/in/PHAR` will not work.
+        var_dump(__FILE__);
+        var_dump($_SERVER['argv']);
+        var_dump($_SERVER['argc']);
+        die;
+
+        require_once 'phar://box.phar/vendor/humbug/php-scoper/src/functions.php';
+
+        return;
     }
 
     throw new RuntimeException('Unable to find the PHP-Scoper functions.');
 };
 
 $bootstrap = function () use ($findAutoloader, $findPhpScoperFunctions): void {
-    require_once $findAutoloader();
-    require_once $findPhpScoperFunctions();
+//    require_once $findAutoloader();
+    $findPhpScoperFunctions();
 
     \KevinGH\Box\register_aliases();
 };
 $bootstrap();
 
-$GLOBALS['bootstrap'] = $bootstrap;
+$GLOBALS['_BOX_BOOTSTRAP'] = $bootstrap;
 
 // Convert errors to exceptions
 set_error_handler(
