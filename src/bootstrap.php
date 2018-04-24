7b6cea01ab42079444c1f9b66bfe8d733743d5a4
Théo FIDRY <theo.fidry@gmail.com>
2018-04-24 08:53:50 +0100

    Attempt

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -15,11 +15,20 @@ declare(strict_types=1);
 namespace KevinGH\Box;
 
 use function array_filter;
+use function bin2hex;
+use function copy;
+use function defined;
+use function dirname;
 use ErrorException;
 use function get_declared_classes;
+use Phar;
 use function preg_match;
+use function random_bytes;
+use function register_shutdown_function;
 use RuntimeException;
 use function substr;
+use function sys_get_temp_dir;
+use function unlink;
 use function var_dump;
 
 $findAutoloader = function () {
@@ -63,15 +72,26 @@ $findPhpScoperFunctions = function (): void {
         return;
     }
 
-    if ('phar:' === substr(__FILE__, 0, 5)) {
+    if ('phar://' === substr(__FILE__, 0, 7)) {
         // Is in the PHAR but the PHAR has been renamed without the extension `.phar`. As a result the PHAR protocol
         // `phar://path/to/file/in/PHAR` will not work.
-        var_dump(__FILE__);
-        var_dump($_SERVER['argv']);
-        var_dump($_SERVER['argc']);
-        die;
+        // See https://github.com/amphp/parallel/commit/732694688461936bec02c0ccf020dfee10c4f7ee
+        if (defined('PHAR_COPY')) {
+            return;
+        }
 
-        require_once 'phar://box.phar/vendor/humbug/php-scoper/src/functions.php';
+        $pharPath = dirname(substr(__FILE__, 7), 2);
+        define('PHAR_COPY', sys_get_temp_dir().'/phar-'.bin2hex(random_bytes(10)). '.phar');
+
+        copy($pharPath, \PHAR_COPY);
+
+        $autoload = 'phar://'.\PHAR_COPY. '/vendor/humbug/php-scoper/src/functions.php';
+
+        register_shutdown_function(static function () {
+            @unlink(\PHAR_COPY);
+        });
+
+        require_once $autoload;
 
         return;
     }
