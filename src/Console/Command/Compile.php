b7fb20d21911c95ef08d9c4f3a7ebec18255501f
Théo FIDRY <theo.fidry@gmail.com>
2018-05-05 16:20:03 +0100

    Update Compile.php

--- src/Console/Command/Compile.php
+++ src/Console/Command/Compile.php
@@ -592,7 +592,7 @@ EOF
                 $logger->log(
                     BuildLogger::CHEVRON_PREFIX,
                     sprintf(
-                        '<info>Warning: the extension "%s" will not be required to execute the PHAR</info>',
+                        '<info>Warning: the extension "%s" will now be required to execute the PHAR</info>',
                         $extension
                     )
                 );
