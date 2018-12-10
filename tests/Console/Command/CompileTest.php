34b2db90d443e0729c226bc76de2d5f6c5ef3246
Théo FIDRY <theo.fidry@gmail.com>
2018-12-10 13:25:29 +0100

    Fix CS

--- tests/Console/Command/CompileTest.php
+++ tests/Console/Command/CompileTest.php
@@ -19,7 +19,6 @@ use Generator;
 use InvalidArgumentException;
 use KevinGH\Box\Compactor\Php;
 use KevinGH\Box\Console\DisplayNormalizer;
-use function KevinGH\Box\get_box_version;
 use KevinGH\Box\Test\CommandTestCase;
 use KevinGH\Box\Test\RequiresPharReadonlyOff;
 use Phar;
