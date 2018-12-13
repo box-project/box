bb4e29e385213fcbdd426832403174d7abbcf2fb
Théo FIDRY <theo.fidry@gmail.com>
2018-12-13 08:50:42 +0100

    Fixed requirement checker

--- requirement-checker/bin/check-requirements.php
+++ requirement-checker/bin/check-requirements.php
@@ -14,7 +14,7 @@ namespace KevinGH\RequirementChecker;
 
 require __DIR__.'/../vendor/autoload.php';
 
-if (false === in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true)) {
+if (false === in_array(\PHP_SAPI, array('cli', 'phpdbg', 'embed'), true)) {
     echo \PHP_EOL.'The application may only be invoked from a command line, got "'.\PHP_SAPI.'"'.PHP_EOL;
 
     exit(1);
