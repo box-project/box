ff8854bc1201b2e883a31af1a8354e7fc3d69779
Théo FIDRY <theo.fidry@gmail.com>
2018-12-13 09:28:45 +0100

    Allow the requirement checker to run with embed SAPI (#340)
    $this->tmp.'/index.phar'

--- requirement-checker/bin/check-requirements.php
+++ requirement-checker/bin/check-requirements.php
@@ -14,8 +14,8 @@ namespace KevinGH\RequirementChecker;
 
 require __DIR__.'/../vendor/autoload.php';
 
-if (\PHP_SAPI !== 'cli' && \PHP_SAPI !== 'phpdbg') {
-    echo \PHP_EOL.'The application may only be invoked from a command line'.\PHP_EOL;
+if (false === in_array(\PHP_SAPI, array('cli', 'phpdbg', 'embed'), true)) {
+    echo \PHP_EOL.'The application may only be invoked from a command line, got "'.\PHP_SAPI.'"'.PHP_EOL;
 
     exit(1);
 }
