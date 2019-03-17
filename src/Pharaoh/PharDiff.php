fa72a9b4e52168b07ed2dc802a00953a38d1fe89
Théo FIDRY <theo.fidry@gmail.com>
2019-03-17 16:09:36 +0100

    Implement various Pharaoh improvements
    - Switch to custom classes to extend Pharaoh's behaviour
    - Destroy the temporary directory created (cf. https://github.com/paragonie/pharaoh/issues/12)
    - Replace the temporary file names used for the diff by the file basename for better readability
      (https://github.com/paragonie/pharaoh/issues/13)

---
+++ src/Pharaoh/PharDiff.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the box project.
+ *
+ * (c) Kevin Herrera <kevin@herrera.io>
+ *     Théo Fidry <theo.fidry@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace KevinGH\Box\Pharaoh;
+
+use function array_map;
+use function escapeshellarg;
+use ParagonIE\Pharaoh\PharDiff as ParagoniePharDiff;
+use function realpath;
+use function str_replace;
+
+final class PharDiff
+{
+    /** @var ParagoniePharDiff */
+    private $diff;
+
+    /** @var Pharaoh */
+    private $pharA;
+
+    /** @var Pharaoh */
+    private $pharB;
+
+    public function __construct(string $pathA, string $pathB)
+    {
+        $phars = array_map(
+            static function (string $path): Pharaoh {
+                $realPath = realpath($path);
+
+                return new Pharaoh(false !== $realPath ? $realPath : $path);
+            },
+            [$pathA, $pathB]
+        );
+
+        $this->pharA = $phars[0];
+        $this->pharB = $phars[1];
+
+        $diff = new ParagoniePharDiff(...$phars);
+        $diff->setVerbose(true);
+
+        $this->diff = $diff;
+    }
+
+    public function gitDiff(): ?string
+    {
+        $argA = escapeshellarg($this->pharA->tmp);
+        $argB = escapeshellarg($this->pharB->tmp);
+
+        /** @var string $diff */
+        $diff = `git diff --no-index $argA $argB`;
+
+        $diff = str_replace(
+            $this->pharA->tmp,
+            $this->pharA->getFileName(),
+            $diff
+        );
+        $diff = str_replace(
+            $this->pharB->tmp,
+            $this->pharB->getFileName(),
+            $diff
+        );
+
+        return '' === $diff ? null : $diff;
+    }
+
+    public function gnuDiff(): ?string
+    {
+        $argA = escapeshellarg($this->pharA->tmp);
+        $argB = escapeshellarg($this->pharB->tmp);
+
+        /** @var string $diff */
+        $diff = `diff $argA $argB`;
+
+        $diff = str_replace(
+            $this->pharA->tmp,
+            $this->pharA->getFileName(),
+            $diff
+        );
+        $diff = str_replace(
+            $this->pharB->tmp,
+            $this->pharB->getFileName(),
+            $diff
+        );
+
+        return '' === $diff ? null : $diff;
+    }
+
+    /**
+     * @see ParagoniePharDiff::listChecksums()
+     */
+    public function listChecksums(string $algo = 'sha384'): int
+    {
+        return $this->diff->listChecksums($algo);
+    }
+}
---
+++ src/Pharaoh/Pharaoh.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the box project.
+ *
+ * (c) Kevin Herrera <kevin@herrera.io>
+ *     Théo Fidry <theo.fidry@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace KevinGH\Box\Pharaoh;
+
+use function basename;
+use function KevinGH\Box\FileSystem\remove;
+use ParagonIE\Pharaoh\Pharaoh as ParagoniePharaoh;
+
+final class Pharaoh extends ParagoniePharaoh
+{
+    private $fileName;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct(string $file, ?string $alias = null)
+    {
+        parent::__construct($file, $alias);
+
+        $this->fileName = basename($file);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __destruct()
+    {
+        parent::__destruct();
+
+        remove($this->tmp);
+    }
+
+    public function getFileName(): string
+    {
+        return $this->fileName;
+    }
+}
