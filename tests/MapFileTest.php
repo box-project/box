b15257900b4544217cb350a73019e792fed6b775
Théo FIDRY <theo.fidry@gmail.com>
2019-05-15 23:18:42 +0200

    Add test for MapFile

--- tests/MapFileTest.php
+++ tests/MapFileTest.php
@@ -2,16 +2,6 @@
 
 declare(strict_types=1);
 
-/*
- * This file is part of the box project.
- *
- * (c) Kevin Herrera <kevin@herrera.io>
- *     Théo Fidry <theo.fidry@gmail.com>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 namespace KevinGH\Box;
 
 use Generator;
@@ -54,7 +44,7 @@ class MapFileTest extends TestCase
         yield [
             $basePath,
             [
-                ['' => 'local/path'],
+                ['' => 'local/path']
             ],
             'foo',
             'local/path/foo',
@@ -63,7 +53,7 @@ class MapFileTest extends TestCase
         yield [
             $basePath,
             [
-                ['foo' => 'local/path/foo'],
+                ['foo' => 'local/path/foo']
             ],
             'foo',
             'local/path/foo',
