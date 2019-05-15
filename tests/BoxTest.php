446972337c991158b8c6a1f08d960665009c07fd
Théo FIDRY <theo.fidry@gmail.com>
2019-05-16 00:20:35 +0200

    Add comment

--- tests/BoxTest.php
+++ tests/BoxTest.php
@@ -59,7 +59,9 @@ use function trim;
 /**
  * @covers \KevinGH\Box\Box
  *
- * @runTestsInSeparateProcesses
+ * @runTestsInSeparateProcesses Although the tear down should be enough, it appears there is some side-efects remaining
+ *                              screwing up with the openssl API. Indeed without this, running this test and OpenSslTest
+ *                              will result in a failure.
  */
 class BoxTest extends FileSystemTestCase
 {
