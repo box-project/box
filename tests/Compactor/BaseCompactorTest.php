f3fd6d02ea84e30a7a15f2d93e916b62ce79af23
Théo FIDRY <theo.fidry@gmail.com>
2019-05-18 06:06:44 +0200

    Fix CS

--- tests/Compactor/BaseCompactorTest.php
+++ tests/Compactor/BaseCompactorTest.php
@@ -14,10 +14,9 @@ declare(strict_types=1);
 
 namespace KevinGH\Box\Compactor;
 
-use;
+use function func_get_args;
 use KevinGH\Box\NotCallable;
 use PHPUnit\Framework\TestCase;
-use function func_get_args;
 
 /**
  * @covers \KevinGH\Box\Compactor\BaseCompactor
