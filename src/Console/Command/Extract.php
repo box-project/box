f1251904ef0ff364b969fe5b1357815375d00865
Théo FIDRY <theo.fidry@gmail.com>
2019-06-06 09:15:12 +0200

    Rework the command

--- src/Console/Command/Extract.php
+++ src/Console/Command/Extract.php
@@ -14,43 +14,23 @@ declare(strict_types=1);
 
 namespace KevinGH\Box\Console\Command;
 
-use function array_filter;
-use function array_flip;
-use Assert\Assertion;
-use DirectoryIterator;
-use function file_get_contents;
-use function getcwd;
-use function is_array;
 use KevinGH\Box\Box;
-use KevinGH\Box\Console\FileDescriptorManipulator;
-use KevinGH\Box\Console\PharInfoRenderer;
+use function KevinGH\Box\bump_open_file_descriptor_limit;
+use KevinGH\Box\Console\IO\IO;
+use function KevinGH\Box\create_temporary_phar;
+use function KevinGH\Box\FileSystem\dump_file;
 use function KevinGH\Box\FileSystem\remove;
-use function KevinGH\Box\format_size;
-use function KevinGH\Box\get_phar_compression_algorithms;
-use KevinGH\Box\PharInfo\PharInfo;
-use Phar;
-use PharData;
 use PharFileInfo;
 use function realpath;
 use RuntimeException;
 use function sprintf;
-use function str_repeat;
-use function str_replace;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
-use Throwable;
-use function var_dump;
 
 /**
  * @private
  */
-final class Extract extends Command
+final class Extract extends BaseCommand
 {
-    use CreateTemporaryPharFile;
-
     private const PHAR_ARG = 'phar';
     private const OUTPUT_ARG = 'output';
 
@@ -78,10 +58,9 @@ final class Extract extends Command
     /**
      * {@inheritdoc}
      */
-    public function execute(InputInterface $input, OutputInterface $output): int
+    protected function executeCommand(IO $io): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $io->newLine();
+        $input = $io->getInput();
 
         $file = realpath($input->getArgument(self::PHAR_ARG));
 
@@ -96,14 +75,24 @@ final class Extract extends Command
             return 1;
         }
 
-        $tmpFile = $this->createTemporaryPhar($file);
+        $tmpFile = create_temporary_phar($file);
 
         $box = Box::create($tmpFile);
 
-        $restoreLimit = FileDescriptorManipulator::bumpOpenFileDescriptorLimit(count($box), $io);
+        $restoreLimit = bump_open_file_descriptor_limit($box, $io);
+
+        $outputDir = $input->getArgument(self::OUTPUT_ARG);
 
         try {
-            $box->compress(Phar::NONE);
+            remove($outputDir);
+
+            foreach ($box->getPhar() as $pharFile) {
+                /* @var PharFileInfo $pharFile */
+                dump_file(
+                    $outputDir.'/'.$pharFile->getFilename(),
+                    (string) $pharFile->getContent()
+                );
+            }
         } catch (RuntimeException $exception) {
             $io->error($exception->getMessage());
 
@@ -114,218 +103,6 @@ final class Extract extends Command
             remove($tmpFile);
         }
 
-        $outputDir = $input->getArgument(self::OUTPUT_ARG);
-
-        $box->getPhar()->decompressFiles();
-
-        unset($box);
-
-        Box::create($tmpFile)->getPhar()->extractTo($outputDir, null, true);
-
         return 0;
-    }
-
-    public function showInfo(string $file, string $originalFile, InputInterface $input, OutputInterface $output, SymfonyStyle $io): int
-    {
-        $depth = (int) $input->getOption(self::DEPTH_OPT);
-
-        Assertion::greaterOrEqualThan($depth, -1, 'Expected the depth to be a positive integer or -1, got "%d"');
-
-        try {
-            $pharInfo = new PharInfo($file);
-
-            return $this->showPharInfo(
-                $pharInfo,
-                $input->getOption(self::OUTPUT_ARG),
-                $depth,
-                'indent' === $input->getOption(self::MODE_OPT),
-                $output,
-                $io
-            );
-        } catch (Throwable $throwable) {
-            if ($output->isDebug()) {
-                throw $throwable;
-            }
-
-            $io->error(
-                sprintf(
-                    'Could not read the file "%s".',
-                    $originalFile
-                )
-            );
-
-            return 1;
-        }
-    }
-
-    private function showGlobalInfo(OutputInterface $output, SymfonyStyle $io): int
-    {
-        $this->render(
-            $output,
-            [
-                'API Version' => Phar::apiVersion(),
-                'Supported Compression' => Phar::getSupportedCompression(),
-                'Supported Signatures' => Phar::getSupportedSignatures(),
-            ]
-        );
-
-        $io->newLine();
-        $io->comment('Get a PHAR details by giving its path as an argument.');
-
-        return 0;
-    }
-
-    private function showPharInfo(
-        PharInfo $pharInfo,
-        bool $content,
-        int $depth,
-        bool $indent,
-        OutputInterface $output,
-        SymfonyStyle $io
-    ): int {
-        $this->showPharMeta($pharInfo, $io);
-
-        if ($content) {
-            $this->renderContents(
-                $output,
-                $pharInfo->getPhar(),
-                0,
-                $depth,
-                $indent ? 0 : false,
-                $pharInfo->getRoot(),
-                $pharInfo->getPhar(),
-                $pharInfo->getRoot()
-            );
-        } else {
-            $io->comment('Use the <info>--list|-l</info> option to list the content of the PHAR.');
-        }
-
-        return 0;
-    }
-
-    private function showPharMeta(PharInfo $pharInfo, SymfonyStyle $io): void
-    {
-        $io->writeln(
-            sprintf(
-                '<comment>API Version:</comment> %s',
-                $pharInfo->getVersion()
-            )
-        );
-
-        $io->newLine();
-
-        PharInfoRenderer::renderCompression($pharInfo, $io);
-
-        $io->newLine();
-
-        PharInfoRenderer::renderSignature($pharInfo, $io);
-
-        $io->newLine();
-
-        PharInfoRenderer::renderMetadata($pharInfo, $io);
-
-        $io->newLine();
-
-        PharInfoRenderer::renderContentsSummary($pharInfo, $io);
-    }
-
-    private function render(OutputInterface $output, array $attributes): void
-    {
-        $out = false;
-
-        foreach ($attributes as $name => $value) {
-            if ($out) {
-                $output->writeln('');
-            }
-
-            $output->write("<comment>$name:</comment>");
-
-            if (is_array($value)) {
-                $output->writeln('');
-
-                foreach ($value as $v) {
-                    $output->writeln("  - $v");
-                }
-            } else {
-                $output->writeln(" $value");
-            }
-
-            $out = true;
-        }
-    }
-
-    /**
-     * @param iterable|PharFileInfo[] $list
-     * @param false|int               $indent Nbr of indent or `false`
-     * @param Phar|PharData           $phar
-     */
-    private function renderContents(
-        OutputInterface $output,
-        iterable $list,
-        int $depth,
-        int $maxDepth,
-        $indent,
-        string $base,
-        $phar,
-        string $root
-    ): void {
-        if (-1 !== $maxDepth && $depth > $maxDepth) {
-            return;
-        }
-
-        foreach ($list as $item) {
-            $item = $phar[str_replace($root, '', $item->getPathname())];
-
-            if (false !== $indent) {
-                $output->write(str_repeat(' ', $indent));
-
-                $path = $item->getFilename();
-
-                if ($item->isDir()) {
-                    $path .= '/';
-                }
-            } else {
-                $path = str_replace($base, '', $item->getPathname());
-            }
-
-            if ($item->isDir()) {
-                if (false !== $indent) {
-                    $output->writeln("<info>$path</info>");
-                }
-            } else {
-                $compression = '<fg=red>[NONE]</fg=red>';
-
-                foreach (self::$FILE_ALGORITHMS as $code => $name) {
-                    if ($item->isCompressed($code)) {
-                        $compression = "<fg=cyan>[$name]</fg=cyan>";
-                        break;
-                    }
-                }
-
-                $fileSize = format_size($item->getCompressedSize());
-
-                $output->writeln(
-                    sprintf(
-                        '%s %s - %s',
-                        $path,
-                        $compression,
-                        $fileSize
-                    )
-                );
-            }
-
-            if ($item->isDir()) {
-                $this->renderContents(
-                    $output,
-                    new DirectoryIterator($item->getPathname()),
-                    $depth + 1,
-                    $maxDepth,
-                    false === $indent ? $indent : $indent + 2,
-                    $base,
-                    $phar,
-                    $root
-                );
-            }
-        }
     }
 }
--- src/functions.php
+++ src/functions.php
@@ -25,6 +25,7 @@ use function define;
 use function defined;
 use ErrorException;
 use function floor;
+use function function_exists;
 use KevinGH\Box\Console\IO\IO;
 use KevinGH\Box\Console\Php\PhpSettingsHandler;
 use function KevinGH\Box\FileSystem\copy;
@@ -32,6 +33,8 @@ use function log;
 use function number_format;
 use PackageVersions\Versions;
 use Phar;
+use function posix_getrlimit;
+use function posix_setrlimit;
 use function random_bytes;
 use function sprintf;
 use function str_replace;
@@ -39,6 +42,7 @@ use function strlen;
 use function strtolower;
 use Symfony\Component\Console\Helper\Helper;
 use Symfony\Component\Console\Logger\ConsoleLogger;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * @private
@@ -263,4 +267,64 @@ function register_error_handler(): void
             }
         }
     );
+}
+
+/**
+ * Bumps the maximum number of open file descriptor if necessary.
+ *
+ * @return Closure callable to call to restore the original maximum number of open files descriptors
+ */
+function bump_open_file_descriptor_limit(Box $box, IO $io): Closure
+{
+    $filesCount = count($box) + 128;  // Add a little extra for good measure
+
+    if (false === function_exists('posix_getrlimit') || false === function_exists('posix_setrlimit')) {
+        $io->writeln(
+            '<info>[debug] Could not check the maximum number of open file descriptors: the functions "posix_getrlimit()" and '
+            .'"posix_setrlimit" could not be found.</info>',
+            OutputInterface::VERBOSITY_DEBUG
+        );
+
+        return static function (): void {};
+    }
+
+    $softLimit = posix_getrlimit()['soft openfiles'];
+    $hardLimit = posix_getrlimit()['hard openfiles'];
+
+    if ($softLimit >= $filesCount) {
+        return static function (): void {};
+    }
+
+    $io->writeln(
+        sprintf(
+            '<info>[debug] Increased the maximum number of open file descriptors from ("%s", "%s") to ("%s", "%s")'
+            .'</info>',
+            $softLimit,
+            $hardLimit,
+            $filesCount,
+            'unlimited'
+        ),
+        OutputInterface::VERBOSITY_DEBUG
+    );
+
+    posix_setrlimit(
+        POSIX_RLIMIT_NOFILE,
+        $filesCount,
+        'unlimited' === $hardLimit ? POSIX_RLIMIT_INFINITY : $hardLimit
+    );
+
+    return static function () use ($io, $softLimit, $hardLimit): void {
+        if (function_exists('posix_setrlimit') && isset($softLimit, $hardLimit)) {
+            posix_setrlimit(
+                POSIX_RLIMIT_NOFILE,
+                $softLimit,
+                'unlimited' === $hardLimit ? POSIX_RLIMIT_INFINITY : $hardLimit
+            );
+
+            $io->writeln(
+                '<info>[debug] Restored the maximum number of open file descriptors</info>',
+                OutputInterface::VERBOSITY_DEBUG
+            );
+        }
+    };
 }
