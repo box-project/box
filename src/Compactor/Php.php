8575e6a7df9353528a25f4ff1e2381cf98eb5ddc
jrfnl <jrfnl@users.noreply.github.com>
2021-10-10 15:22:12 +0200

    Compactor\Php: add support for PHP 8.0+ attributes
    Up to now, when a Phar was being created using PHP < 8.0 with the PHP compactor enabled, PHP 8.0+ attributes would be incorrectly removed as if they were comments.
    
    This has now been fixed.
    
    Both the solution implemented, as well as the unit tests, are largely inspired by the solution for handling attributes when code is being tokenized on PHP < 8.0, as implemented in `PHP_CodeSniffer` in https://github.com/squizlabs/PHP_CodeSniffer/pull/3203 and https://github.com/squizlabs/PHP_CodeSniffer/pull/3299. Props to alekitto.
    
    Fixes 567
    
    Co-authored-by: Alessandro Chitolina <alekitto@gmail.com>

--- src/Compactor/Php.php
+++ src/Compactor/Php.php
@@ -14,9 +14,15 @@ declare(strict_types=1);
 
 namespace KevinGH\Box\Compactor;
 
+use function array_pop;
+use function array_slice;
+use function array_splice;
 use function count;
+use function define;
+use function defined;
 use function in_array;
 use function is_array;
+use function is_int;
 use function is_string;
 use KevinGH\Box\Annotation\DocblockAnnotationParser;
 use KevinGH\Box\Annotation\InvalidToken;
@@ -25,6 +31,7 @@ use function preg_replace;
 use RuntimeException;
 use function str_repeat;
 use function strpos;
+use function substr;
 use function substr_count;
 use const T_COMMENT;
 use const T_DOC_COMMENT;
@@ -40,6 +47,8 @@ use function token_get_all;
  * @author Fabien Potencier <fabien@symfony.com>
  * @author Jordi Boggiano <j.boggiano@seld.be>
  * @author Théo Fidry <theo.fidry@gmail.com>
+ * @author Juliette Reinders Folmer <boxproject_nospam@adviesenzo.nl>
+ * @author Alessandro Chitolina <alekitto@gmail.com>
  * @private
  */
 final class Php extends FileExtensionCompactor
@@ -63,12 +72,31 @@ final class Php extends FileExtensionCompactor
     {
         $output = '';
         $tokens = token_get_all($contents);
+        $tokenCount = count($tokens);
 
-        foreach ($tokens as $index => $token) {
+        for ($index = 0; $index < $tokenCount; $index++) {
+            $token = $tokens[$index];
             if (is_string($token)) {
                 $output .= $token;
             } elseif (in_array($token[0], [T_COMMENT, T_DOC_COMMENT], true)) {
-                if (false !== strpos($token[1], '@')) {
+                if (0 === strpos($token[1], '#[')) {
+                    // This is, in all likelyhood, the start of a PHP >= 8.0 attribute.
+                    // Note: $tokens may be updated by reference as well!
+                    $retokenized = $this->retokenizeAttribute($tokens, $index);
+                    if (null !== $retokenized) {
+                        array_splice($tokens, $index, 1, $retokenized);
+                        $tokenCount = count($tokens);
+                    }
+
+                    $attributeCloser = $this->findAttributeCloser($tokens, $index);
+
+                    if (is_int($attributeCloser)) {
+                        $output .= '#[';
+                    } else {
+                        // Turns out this was not an attribute. Treat it as a plain comment.
+                        $output .= str_repeat("\n", substr_count($token[1], "\n"));
+                    }
+                } elseif (false !== strpos($token[1], '@')) {
                     try {
                         $output .= $this->compactAnnotations($token[1]);
                     } catch (InvalidToken $exception) {
@@ -84,17 +112,29 @@ final class Php extends FileExtensionCompactor
                     $output .= str_repeat("\n", substr_count($token[1], "\n"));
                 }
             } elseif (T_WHITESPACE === $token[0]) {
+                $whitespace    = $token[1];
+                $previousIndex = ($index - 1);
+
+                // Handle whitespace potentially being split into two tokens after attribute retokenization.
+                if (isset($tokens[$index + 1])
+                    && is_array($tokens[$index + 1])
+                    && T_WHITESPACE === $tokens[$index + 1][0]
+                ) {
+                    $whitespace .= $tokens[$index + 1][1];
+                    $index++;
+                }
+
                 // reduce wide spaces
-                $whitespace = preg_replace('{[ \t]+}', ' ', $token[1]);
+                $whitespace = preg_replace('{[ \t]+}', ' ', $whitespace);
 
                 // normalize newlines to \n
                 $whitespace = preg_replace('{(?:\r\n|\r|\n)}', "\n", $whitespace);
 
                 // If the new line was split off from the whitespace token due to it being included in
                 // the previous (comment) token (PHP < 8), remove leading spaces.
-                if (is_array($tokens[$index - 1])
-                    && T_COMMENT === $tokens[$index - 1][0]
-                    && strpos($tokens[$index - 1][1], "\n") !== false
+                if (is_array($tokens[$previousIndex])
+                    && T_COMMENT === $tokens[$previousIndex][0]
+                    && strpos($tokens[$previousIndex][1], "\n") !== false
                 ) {
                     $whitespace = ltrim($whitespace, ' ');
                 }
@@ -138,5 +178,74 @@ final class Php extends FileExtensionCompactor
         }
 
         return $compactedDocblock;
+    }
+
+    private function findAttributeCloser(array $tokens, int $opener): ?int
+    {
+        $tokenCount = count($tokens);
+        $brackets   = [$opener];
+        $closer     = null;
+
+        for ($i = ($opener + 1); $i < $tokenCount; $i++) {
+            if (false === is_string($tokens[$i])) {
+                continue;
+            }
+
+            // Allow for short arrays within attributes.
+            if ('[' === $tokens[$i]) {
+                $brackets[] = $i;
+                continue;
+            }
+
+            if (']' === $tokens[$i]) {
+                array_pop($brackets);
+                if (empty($brackets)) {
+                    $closer = $i;
+                    break;
+                }
+            }
+        }
+
+        return $closer;
+    }
+
+    private function retokenizeAttribute(array &$tokens, int $opener): ?array
+    {
+        $token         = $tokens[$opener];
+        $attributeBody = substr($token[1], 2);
+        $subTokens     = @token_get_all('<?php ' . $attributeBody);
+
+        // Replace the PHP open tag with the attribute opener as a simple token.
+        array_splice($subTokens, 0, 1, ['#[']);
+
+        $closer = $this->findAttributeCloser($subTokens, 0);
+
+        // Multi-line attribute or attribute containing something which looks like a PHP close tag.
+        // Retokenize the rest of the file after the attribute opener.
+        if (null === $closer) {
+            foreach (array_slice($tokens, ($opener + 1)) as $token) {
+                if (is_array($token)) {
+                    $attributeBody .= $token[1];
+                } else {
+                    $attributeBody .= $token;
+                }
+            }
+
+            $subTokens = @token_get_all('<?php '.$attributeBody);
+            array_splice($subTokens, 0, 1, ['#[']);
+
+            $closer = $this->findAttributeCloser($subTokens, 0);
+
+            if (null !== $closer) {
+                array_splice($tokens, ($opener + 1), count($tokens), array_slice($subTokens, ($closer + 1)));
+                $subTokens = array_slice($subTokens, 0, ($closer + 1));
+            }
+        }
+
+        if (null === $closer) {
+            return null;
+        }
+
+        return $subTokens;
     }
 }
