9b2dc9d296241e5ce8923c5acc373004d1f7f38f
Théo FIDRY <theo.fidry@gmail.com>
2022-02-20 16:53:15 +0100

    Add Rector

---
+++ rector.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Core\Configuration\Option;
+use Rector\Set\ValueObject\LevelSetList;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+
+    $parameters->set(
+        Option::PATHS,
+        [
+            __DIR__ . '/fixtures',
+            __DIR__ . '/src',
+            __DIR__ . '/tests',
+        ],
+    );
+
+    $parameters->set(Option::AUTOLOAD_PATHS, [
+        __DIR__ . '/vendor/autoload.php',
+        __DIR__ . '/vendor-bin/rector/vendor/autoload.php',
+    ]);
+
+    // Define what rule sets will be applied
+    $containerConfigurator->import(LevelSetList::UP_TO_PHP_81);
+
+    // get services (needed for register a single rule)
+    // $services = $containerConfigurator->services();
+
+    // register a single rule
+    // $services->set(TypedPropertyRector::class);
+};
