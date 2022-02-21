907df699a798943ad61d2c4957c1cc69373736b8
Théo FIDRY <theo.fidry@gmail.com>
2022-02-21 10:31:42 +0100

    Update the requirement checker config

--- requirement-checker/scoper.inc.php
+++ requirement-checker/scoper.inc.php
@@ -25,30 +25,4 @@ function get_prefix(): string
 
 return [
     'prefix' => get_prefix(),
-
-    'expose-global-classes' => false,
-    'expose-global-constants' => false,
-    'expose-global-functions' => false,
-
-    'patchers' => [
-        // TODO: report back the missing sapi_windows_vt100_support to JetBrains stubs
-        static function (string $filePath, string $prefix, string $contents): string {
-            $files = [
-                'vendor/sebastian/environment/src/Console.php',
-                'src/IO.php',
-            ];
-
-            if (false === in_array($filePath, $files, true)) {
-                return $contents;
-            }
-
-            $contents = preg_replace(
-                '/function_exists\(\''.$prefix.'\\\\(\\\\)?sapi_windows_vt100_support\'\)/',
-                "function_exists('sapi_windows_vt100_support')",
-                $contents
-            );
-
-            return $contents;
-        },
-    ],
 ];
