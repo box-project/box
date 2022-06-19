69b7dee208344b28c4fe019ece7586f724dea58a
Théo FIDRY <theo.fidry@gmail.com>
2022-06-20 01:07:49 +0200

    Extract Logo into a dedicated class (#660)

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -24,18 +24,6 @@ use function trim;
  */
 final class Application extends SymfonyApplication
 {
-    private const LOGO = <<<'ASCII'
-
-            ____
-           / __ )____  _  __
-          / __  / __ \| |/_/
-         / /_/ / /_/ />  <
-        /_____/\____/_/|_|
-
-
-
-        ASCII;
-
     private string $releaseDate;
 
     public function __construct(string $name = 'Box', ?string $version = null, string $releaseDate = '@release-date@')
@@ -61,7 +49,7 @@ final class Application extends SymfonyApplication
 
     public function getHelp(): string
     {
-        return self::LOGO.parent::getHelp();
+        return Logo::LOGO_ASCII.parent::getHelp();
     }
 
     protected function getDefaultCommands(): array
