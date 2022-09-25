f5c6ab736e617250318e25d69c71dcb2f810e0b8
Théo FIDRY <theo.fidry@gmail.com>
2022-09-25 12:47:53 +0200

    Fix

--- tests/Configuration/ConfigurationTest.php
+++ tests/Configuration/ConfigurationTest.php
@@ -2977,9 +2977,9 @@ class ConfigurationTest extends ConfigurationTestCase
         $expectedDumpedConfig = <<<EOF
             KevinGH\Box\Configuration\Configuration {#100
               -compressionAlgorithm: "GZ"
-              -mainScriptPath: & "index.php"
+              -mainScriptPath: "index.php"
               -mainScriptContents: ""
-              -file: & "box.json"
+              -file: "box.json"
               -alias: "test.phar"
               -basePath: "/path/to"
               -composerJson: KevinGH\Box\Composer\ComposerFile {#100
@@ -2998,7 +2998,7 @@ class ConfigurationTest extends ConfigurationTestCase
                 -path: "composer.lock"
                 -contents: []
               }
-              -files: & array:6 [
+              -files: array:6 [
                 0 => "bar.php"
                 1 => "box.json"
                 2 => "composer.json"
@@ -3006,7 +3006,7 @@ class ConfigurationTest extends ConfigurationTestCase
                 4 => "foo.php"
                 5 => "vendor/composer/installed.json"
               ]
-              -binaryFiles: & array:2 [
+              -binaryFiles: array:2 [
                 0 => "bar.php"
                 1 => "foo.php"
               ]
@@ -3023,17 +3023,17 @@ class ConfigurationTest extends ConfigurationTestCase
                 -map: []
               }
               -metadata: null
-              -tmpOutputPath: & "index.phar"
-              -outputPath: & "index.phar"
+              -tmpOutputPath: "index.phar"
+              -outputPath: "index.phar"
               -privateKeyPassphrase: null
-              -privateKeyPath: & null
+              -privateKeyPath: null
               -promptForPrivateKey: false
               -processedReplacements: []
               -shebang: "#!/usr/bin/env php"
               -signingAlgorithm: "SHA1"
               -stubBannerContents: "My banner"
-              -stubBannerPath: & null
-              -stubPath: & null
+              -stubBannerPath: null
+              -stubPath: null
               -isInterceptFileFuncs: false
               -isStubGenerated: true
               -checkRequirements: true
@@ -3047,6 +3047,12 @@ class ConfigurationTest extends ConfigurationTestCase
             $this->tmp,
             '/path/to',
             $this->config->export(),
+        );
+
+        $actualDumpedConfig = str_replace(
+            ': & ',
+            ': ',
+            $actualDumpedConfig,
         );
 
         $actualDumpedConfig = preg_replace(
@@ -3083,7 +3089,7 @@ class ConfigurationTest extends ConfigurationTestCase
 
     public static function JsonValidNonStringValuesProvider(): iterable
     {
-        foreach ($this->provideJsonPrimitives() as $key => $value) {
+        foreach (self::provideJsonPrimitives() as $key => $value) {
             if ('string' === $key) {
                 continue;
             }
@@ -3094,7 +3100,7 @@ class ConfigurationTest extends ConfigurationTestCase
 
     public static function JsonValidNonStringArrayProvider(): iterable
     {
-        foreach ($this->provideJsonPrimitives() as $key => $values) {
+        foreach (self::provideJsonPrimitives() as $key => $values) {
             if ('string' === $key) {
                 continue;
             }
@@ -3105,7 +3111,7 @@ class ConfigurationTest extends ConfigurationTestCase
 
     public static function JsonValidNonObjectArrayProvider(): iterable
     {
-        foreach ($this->provideJsonPrimitives() as $key => $values) {
+        foreach (self::provideJsonPrimitives() as $key => $values) {
             if ('object' === $key) {
                 continue;
             }
@@ -3114,7 +3120,7 @@ class ConfigurationTest extends ConfigurationTestCase
         }
     }
 
-    public static function JsonPrimitivesProvider(): iterable
+    public static function provideJsonPrimitives(): iterable
     {
         yield 'null' => null;
         yield 'bool' => true;
