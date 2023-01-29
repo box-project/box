0eb8e7cb4e830e81d6f3d956ec80214ddf4afd8a
Théo FIDRY <theo.fidry@gmail.com>
2023-01-29 18:43:04 +0100

    chore(requirement-checker): Fix the namespace of the auto-review tests

--- requirement-checker/tests/AutoReview/E2EMakefileTest.php
+++ requirement-checker/tests/AutoReview/E2EMakefileTest.php
@@ -12,7 +12,7 @@ declare(strict_types=1);
  * with this source code in the file LICENSE.
  */
 
-namespace AutoReview;
+namespace KevinGH\RequirementChecker\AutoReview;
 
 use Fidry\Makefile\Parser;
 use Fidry\Makefile\Rule;
