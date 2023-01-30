80f672ef9e5b5ce936d53cb63ab3c484cfc42916
Théo FIDRY <theo.fidry@gmail.com>
2023-01-30 19:42:07 +0100

    feat: Allow PHP 8.2 as a docker image

--- src/DockerFileGenerator.php
+++ src/DockerFileGenerator.php
@@ -43,6 +43,7 @@ final class DockerFileGenerator
         Dockerfile;
 
     private const PHP_DOCKER_IMAGES = [
+        '8.2.0' => '8.2-cli-alpine',
         '8.1.0' => '8.1-cli-alpine',
         '8.0.0' => '8.0-cli-alpine',
         '7.4.0' => '7.4-cli-alpine',
