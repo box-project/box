dd19e45cc1f97d809707ddb45b5814df4a0dbb6f
Théo FIDRY <theo.fidry@gmail.com>
2023-02-12 13:30:09 +0100

    dump

--- res/requirement-checker/bin/check-requirements.php
+++ res/requirement-checker/bin/check-requirements.php
@@ -2,24 +2,19 @@
 
 namespace HumbugBox420\KevinGH\RequirementChecker;
 
-use function gettype;
-use function in_array;
-use function is_bool;
-use function is_string;
-use function strtolower;
 if (isset($_SERVER['BOX_REQUIREMENT_CHECKER'])) {
     $enableRequirementChecker = $_SERVER['BOX_REQUIREMENT_CHECKER'];
-    if (is_bool($enableRequirementChecker) && !$enableRequirementChecker) {
+    if (\is_bool($enableRequirementChecker) && !$enableRequirementChecker) {
         return;
     }
-    if (is_string($enableRequirementChecker) && in_array(strtolower($enableRequirementChecker), ['false', '0'], \true)) {
+    if (\is_string($enableRequirementChecker) && \in_array(\strtolower($enableRequirementChecker), ['false', '0'], \true)) {
         return;
     }
-    if (!is_bool($enableRequirementChecker) && !is_string($enableRequirementChecker)) {
-        echo \PHP_EOL . 'Unhandled value type for "BOX_REQUIREMENT_CHECKER". Got "' . gettype($enableRequirementChecker) . '". Proceeding with the requirement checks.' . \PHP_EOL;
+    if (!\is_bool($enableRequirementChecker) && !\is_string($enableRequirementChecker)) {
+        echo \PHP_EOL . 'Unhandled value type for "BOX_REQUIREMENT_CHECKER". Got "' . \gettype($enableRequirementChecker) . '". Proceeding with the requirement checks.' . \PHP_EOL;
     }
 }
-if (\false === in_array(\PHP_SAPI, array('cli', 'phpdbg', 'embed', 'micro'), \true)) {
+if (\false === \in_array(\PHP_SAPI, array('cli', 'phpdbg', 'embed', 'micro'), \true)) {
     echo \PHP_EOL . 'The application may only be invoked from a command line, got "' . \PHP_SAPI . '"' . \PHP_EOL;
     exit(1);
 }
