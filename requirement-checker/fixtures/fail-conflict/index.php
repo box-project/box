490db9bc810ecf6b7b98447b3c60cf557bc2f932
Théo FIDRY <theo.fidry@gmail.com>
2023-02-12 17:28:23 +0100

    add e2e test

---
+++ requirement-checker/fixtures/fail-conflict/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * This file is part of the box project.
+ *
+ * (c) Kevin Herrera <kevin@herrera.io>
+ *     Théo Fidry <theo.fidry@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+throw new Exception('Should not be executed!');
