2ee5243c56b1434df91b56f56c02b99539926054
Théo FIDRY <theo.fidry@gmail.com>
2023-02-17 09:17:01 +0100

    debug

--- src/Composer/ComposerOrchestrator.php
+++ src/Composer/ComposerOrchestrator.php
@@ -150,7 +150,7 @@ final class ComposerOrchestrator
             return;
         }
 
-        $composerCommand = [$composerExecutable, 'install', '--no-dev', '--no-scripts', '--no-plugins'];
+        $composerCommand = [$composerExecutable, 'install', '--no-dev', '--no-scripts', '--no-plugins', '-vvv'];
 
         if (null !== $verbosity = self::retrieveSubProcessVerbosity($logger->getIO())) {
             $composerCommand[] = $verbosity;
