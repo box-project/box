3bc7537babaacec09e52c9f5173eabc2745000d1
Théo FIDRY <theo.fidry@gmail.com>
2023-02-20 23:22:44 +0100

    fix

--- src/Composer/ComposerOrchestrator.php
+++ src/Composer/ComposerOrchestrator.php
@@ -39,9 +39,9 @@ final class ComposerOrchestrator
 {
     use NotInstantiable;
 
-    public static function getVersion(): string
+    public static function getVersion(?string $composerBin = null): string
     {
-        $composerExecutable = self::retrieveComposerExecutable();
+        $composerExecutable = $composerBin ?? self::retrieveComposerExecutable();
         $command = [$composerExecutable, '--version'];
 
         $getVersionProcess = new Process($command);
@@ -69,7 +69,7 @@ final class ComposerOrchestrator
         SymbolsRegistry $symbolsRegistry,
         string $prefix,
         bool $excludeDevFiles,
-        ?string $composerBin,
+        ?string $composerBin = null,
         ?IO $io = null,
     ): void {
         $io ??= IO::createNull();
