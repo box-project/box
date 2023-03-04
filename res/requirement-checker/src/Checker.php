abe3e885f124a5d54f4852e1169a4960b60f6041
theofidry <theofidry@users.noreply.github.com>
2023-03-04 10:11:12 +0000

    Update the RequirementChecker

--- res/requirement-checker/src/Checker.php
+++ res/requirement-checker/src/Checker.php
@@ -1,7 +1,7 @@
 <?php
 
 declare (strict_types=1);
-namespace HumbugBox431\KevinGH\RequirementChecker;
+namespace HumbugBox435\KevinGH\RequirementChecker;
 
 use InvalidArgumentException;
 use function count;
--- res/requirement-checker/vendor/composer/autoload_psr4.php
+++ res/requirement-checker/vendor/composer/autoload_psr4.php
@@ -6,6 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'HumbugBox431\\KevinGH\\RequirementChecker\\' => array($baseDir . '/src'),
-    'HumbugBox431\\Composer\\Semver\\' => array($vendorDir . '/composer/semver/src'),
+    'HumbugBox435\\KevinGH\\RequirementChecker\\' => array($baseDir . '/src'),
+    'HumbugBox435\\Composer\\Semver\\' => array($vendorDir . '/composer/semver/src'),
 );
