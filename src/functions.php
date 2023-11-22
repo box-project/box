e0c5508a5c203b441c0ea360bbda9457d86f0881
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2023-11-22 10:08:34 +0100

    refactor: Move the bump of the file descriptor limit to a dedicated class (#1189)

--- src/functions.php
+++ src/functions.php
@@ -22,7 +22,6 @@ use KevinGH\Box\Console\Php\PhpSettingsHandler;
 use Phar;
 use Symfony\Component\Console\Helper\Helper;
 use Symfony\Component\Console\Logger\ConsoleLogger;
-use Symfony\Component\Console\Output\OutputInterface;
 use Webmozart\Assert\Assert;
 use function bin2hex;
 use function class_alias;
@@ -31,18 +30,13 @@ use function constant;
 use function define;
 use function defined;
 use function floor;
-use function function_exists;
 use function is_float;
 use function is_int;
 use function log;
 use function number_format;
-use function posix_getrlimit;
-use function posix_setrlimit;
 use function random_bytes;
 use function sprintf;
 use function str_replace;
-use const POSIX_RLIMIT_INFINITY;
-use const POSIX_RLIMIT_NOFILE;
 
 /**
  * @private
@@ -240,64 +234,4 @@ function register_error_handler(): void
             }
         },
     );
-}
-
-/**
- * Bumps the maximum number of open file descriptor if necessary.
- *
- * @return Closure callable to call to restore the original maximum number of open files descriptors
- */
-function bump_open_file_descriptor_limit(int $count, IO $io): Closure
-{
-    $count += 128;  // Add a little extra for good measure
-
-    if (false === function_exists('posix_getrlimit') || false === function_exists('posix_setrlimit')) {
-        $io->writeln(
-            '<info>[debug] Could not check the maximum number of open file descriptors: the functions "posix_getrlimit()" and '
-            .'"posix_setrlimit" could not be found.</info>',
-            OutputInterface::VERBOSITY_DEBUG,
-        );
-
-        return static function (): void {};
-    }
-
-    $softLimit = posix_getrlimit()['soft openfiles'];
-    $hardLimit = posix_getrlimit()['hard openfiles'];
-
-    if ($softLimit >= $count) {
-        return static function (): void {};
-    }
-
-    $io->writeln(
-        sprintf(
-            '<info>[debug] Increased the maximum number of open file descriptors from ("%s", "%s") to ("%s", "%s")'
-            .'</info>',
-            $softLimit,
-            $hardLimit,
-            $count,
-            'unlimited',
-        ),
-        OutputInterface::VERBOSITY_DEBUG,
-    );
-
-    posix_setrlimit(
-        POSIX_RLIMIT_NOFILE,
-        $count,
-        'unlimited' === $hardLimit ? POSIX_RLIMIT_INFINITY : $hardLimit,
-    );
-
-    return static function () use ($io, $softLimit, $hardLimit): void {
-        if (function_exists('posix_setrlimit') && isset($softLimit, $hardLimit)) {
-            posix_setrlimit(
-                POSIX_RLIMIT_NOFILE,
-                $softLimit,
-                'unlimited' === $hardLimit ? POSIX_RLIMIT_INFINITY : $hardLimit,
-            );
-
-            $io->writeln(
-                '<info>[debug] Restored the maximum number of open file descriptors</info>',
-                OutputInterface::VERBOSITY_DEBUG,
-            );
-        }
-    };
 }
