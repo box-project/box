c3eb746e0e5b2d08c27fef3a9963da5d5d0c9e57
Théo FIDRY <theo.fidry@gmail.com>
2023-11-26 10:33:59 +0100

    refactor: Extract the parallel processing in a different class

---
+++ src/Parallelization/ParallelizationDecider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the box project.
+ *
+ * (c) Kevin Herrera <kevin@herrera.io>
+ *     Théo Fidry <theo.fidry@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace KevinGH\Box\Parallelization;
+
+use KevinGH\Box\NotInstantiable;
+use KevinGH\Box\PhpScoper\NullScoper;
+use KevinGH\Box\PhpScoper\Scoper;
+
+/**
+ * @private
+ */
+final class ParallelizationDecider
+{
+    use NotInstantiable;
+
+    private const MIN_FILE_COUNT = 500;
+
+    public static function shouldProcessFilesInParallel(
+        Scoper $scoper,
+        int $filesCount,
+    ): bool {
+        return !($scoper instanceof NullScoper || $filesCount < self::MIN_FILE_COUNT);
+    }
+}
