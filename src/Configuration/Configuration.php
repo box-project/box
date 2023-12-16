0a76ccc0399d320ce1763aed404e123a28fb4a50
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2023-12-16 16:57:52 +0100

    fix: Do not refer to the composer.json file when decoding the composer.lock file (#1289)
    fix: Do not refer to the composer.json file when decoding the
    composer.lock file

--- src/Configuration/Configuration.php
+++ src/Configuration/Configuration.php
@@ -1830,7 +1830,7 @@ final class Configuration
         } catch (ParsingException $exception) {
             throw new InvalidArgumentException(
                 sprintf(
-                    'Expected the file "%s" to be a valid composer.json file but an error has been found: %s',
+                    'Expected the file "%s" to be a valid JSON file but an error has been found: %s',
                     $path,
                     $exception->getMessage(),
                 ),
