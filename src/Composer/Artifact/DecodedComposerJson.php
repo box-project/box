e2cdebf8bc3d2b4543337e41ca9a284678f39f0f
Théo FIDRY <5175937+theofidry@users.noreply.github.com>
2023-12-16 12:16:24 +0100

    refactor: Rename the properties of the decoded composer artifacts (#1278)

--- src/Composer/Artifact/DecodedComposerJson.php
+++ src/Composer/Artifact/DecodedComposerJson.php
@@ -25,15 +25,15 @@ use function array_keys;
 final readonly class DecodedComposerJson
 {
     /**
-     * @param array $composerJsonDecodedContents Decoded JSON contents of the `composer.json` file
+     * @param array $decodedContents Decoded JSON contents of the `composer.json` file
      */
-    public function __construct(private array $composerJsonDecodedContents)
+    public function __construct(private array $decodedContents)
     {
     }
 
     public function getRequiredPhpVersion(): ?string
     {
-        return $this->composerJsonDecodedContents['require']['php'] ?? null;
+        return $this->decodedContents['require']['php'] ?? null;
     }
 
     public function hasRequiredPhpVersion(): bool
@@ -42,11 +42,11 @@ final readonly class DecodedComposerJson
     }
 
     /**
-     * @return RequiredItem
+     * @return RequiredItem[]
      */
     public function getRequiredItems(): array
     {
-        $require = $this->composerJsonDecodedContents['require'] ?? [];
+        $require = $this->decodedContents['require'] ?? [];
 
         return array_map(
             static fn (string $packageName) => new RequiredItem([$packageName => $require[$packageName]]),
@@ -57,7 +57,7 @@ final readonly class DecodedComposerJson
     public function getConflictingExtensions(): Extensions
     {
         return PackageInfo::parseExtensions(
-            $this->composerJsonDecodedContents['conflict'] ?? [],
+            $this->decodedContents['conflict'] ?? [],
         );
     }
 }
