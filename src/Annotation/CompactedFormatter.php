225dd49bc8d8243f85b59da86e7c0a13b9a151c6
Théo FIDRY <theo.fidry@gmail.com>
2024-04-23 21:13:51 +0200

    build(deps): Upgrade to phpdocumentor/reflection-docblock 5.4

--- src/Annotation/CompactedFormatter.php
+++ src/Annotation/CompactedFormatter.php
@@ -17,18 +17,38 @@ namespace KevinGH\Box\Annotation;
 use phpDocumentor\Reflection\DocBlock\Tag;
 use phpDocumentor\Reflection\DocBlock\Tags\Formatter;
 use phpDocumentor\Reflection\DocBlock\Tags\Generic;
+use phpDocumentor\Reflection\DocBlock\Tags\InvalidTag;
 use function array_map;
 use function explode;
 use function implode;
+use function sprintf;
 
 final class CompactedFormatter implements Formatter
 {
     public function format(Tag $tag): string
     {
-        if (!$tag instanceof Generic) {
-            return trim('@'.$tag->getName());
+        if ($tag instanceof InvalidTag) {
+            return self::formatInvalidTag($tag);
         }
 
+        if ($tag instanceof Generic) {
+            return self::formatGenericTag($tag);
+        }
+
+        return trim('@'.$tag->getName());
+    }
+
+    private static function formatInvalidTag(InvalidTag $tag): string
+    {
+        return sprintf(
+            '@%s %s',
+            $tag->getName(),
+            $tag,
+        );
+    }
+
+    private static function formatGenericTag(Generic $tag): string
+    {
         $description = (string) $tag;
 
         if (!str_starts_with($description, '(')) {
